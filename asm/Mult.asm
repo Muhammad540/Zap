17d70ae372250cf07b7f7e7c5a81c7d9ce4e7771
Muhammad540 <thisisdeahmed@gmail.com>
2026-02-14 22:42:08 +0100

    example using mc

---
+++ asm/Mult.asm
@@ -0,0 +1,39 @@
+// Multiplies R0 and R1 and stores the result in R2.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+// The algorithm is based on repetitive addition.
+
+// Store R0 in a
+@R0
+D=M
+@a
+M=D
+// Store R1 in b
+@R1
+D=M
+@b
+M=D
+
+@R2
+M=0
+
+(LOOP)
+// if b == 0; EXIT the loop
+@b
+D=M
+@END
+D;JEQ
+// add a to R2
+@a
+D=M
+@R2
+M=D+M
+// dec b
+@b 
+M=M-1
+D=M
+@LOOP
+D;JNE
+
+(END)
+@END
+0;JMP
